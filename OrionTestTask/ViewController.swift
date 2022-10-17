7c38d893263498aaddf21b7066afef0a3fcadef3
Junda Ai <39738304+Alan052918@users.noreply.github.com>
2022-10-16 23:25:44 -0400

    Initial Commit

---
+++ OrionTestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OrionTestTask
+//
+//  Created by Junda Ai on 10/16/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
