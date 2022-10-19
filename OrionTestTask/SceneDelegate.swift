b3b59bc43b17531e0b3c8d170a3d0a16c1356942
Junda Ai <39738304+Alan052918@users.noreply.github.com>
2022-10-19 01:57:35 -0400

    Add navigationController under ViewController

--- OrionTestTask/SceneDelegate.swift
+++ OrionTestTask/SceneDelegate.swift
@@ -20,9 +20,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
-
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = ViewController()
+        let viewController = ViewController(nibName: nil, bundle: nil)
+        let navigationController = UINavigationController(rootViewController: viewController)
+        window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
     }
 
